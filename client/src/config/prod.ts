a05c643340feed53ee4b838602b27f04fe9e202b
magpia13 <30365499+magpia13@users.noreply.github.com>
2021-11-24 13:22:12 +0100

    Update prod.ts

--- client/src/config/prod.ts
+++ client/src/config/prod.ts
@@ -1,15 +1,5 @@
 export const keysProd: {
   BASE_URL: string | undefined;
-  //   url: string;
-  //   user: string;
-  //   pass: string;
-  //   secret: string;
-  //   jwtSecret: string;
 } = {
   BASE_URL: process.env.BASE_URL,
-  //   url: process.env.URL,
-  //   user: process.env.USER,
-  //   pass: process.env.PASS,
-  //   secret: process.env.SECRET,
-  //   jwtSecret: process.env.SECRETJWT,
 };
