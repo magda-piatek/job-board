22a3af48d4bf916ae9d0e452b850f9516dd58acb
magpia13 <30365499+magpia13@users.noreply.github.com>
2021-11-24 13:21:50 +0100

    Update prod.ts

--- server/config/prod.ts
+++ server/config/prod.ts
@@ -1,15 +1,7 @@
 export const keysProd: {
   mongoURI: string | undefined;
-  //   url: string;
-  //   user: string;
-  //   pass: string;
-  //   secret: string;
   jwtSecret: string;
 } = {
   mongoURI: process.env.MONGO_URI,
-  //   url: process.env.URL,
-  //   user: process.env.USER,
-  //   pass: process.env.PASS,
-  //   secret: process.env.SECRET,
   jwtSecret: process.env.SECRETJWT,
 };
